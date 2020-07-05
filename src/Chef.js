8cbcf209f5e11eb31feeb4dd86985ab4a8d780f3
Kristi Miller <kristi.ann.miller@gmail.com>
2020-07-05 11:05:21 -0600

    Simplify checkForFood method

--- src/Chef.js
+++ src/Chef.js
@@ -18,47 +18,59 @@ class Chef {
   // METHOD checkForFood checks to see if an item is on the menu, returns a string
   // checks to see if an item is not on the menu, returns a different string
 
-  // create METHOD checkForFood that takes in params of foodItem, and restaurant object
-  checkForFood(foodItem, restaurant) {
-    // assign restaurant to this chef's restaurant
-    restaurant = this.restaurant
+  // create METHOD checkForFood that takes in param of foodItem
+  checkForFood(foodItem) {
+    // IF any of the menus within THIS instance of the class INCLUDES the param
+    if(this.restaurant.menus.breakfast.includes(foodItem) || this.restaurant.menus.lunch.includes(foodItem) || this.restaurant.menus.dinner.includes(foodItem)) {
+      // RETURN INTERPOLATED STRING
+      return `Yes, we're serving ${foodItem.name} today!`
+      // ELSE RETURN other INTERPOLATED STRING
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    };
+  };
+};
     // create empty ARRAY to push to
-    var menuSearch = []
+    // var menuSearch = []
     // create FUNCTION to check through the menu of the restaurant param
-    function checkMenu(restaurant) {
+    // function checkMenus(foodItem) {
       // IF there is a value within the breakfast menu of the object
-      if(restaurant.menus.breakfast[0]) {
+      /*if(this.restaurant.menus.breakfast) {
         // assign the variable bkfst to the name property of that value
-        var bkfst = restaurant.menus.breakfast[0].name
+        var bkfst = this.restaurant.menus.breakfast[0].name
         // ELSE assign the variable bkfst to an empty array
       } else {
         var bkfst = []
       };
       // repeat IF/ELSE statments for lunch and dinner menus
-      if(restaurant.menus.lunch[0]) {
-        var lunch = restaurant.menus.lunch[0].name
+      if(this.restaurant.menus.lunch[0]) {
+        var lunch = this.restaurant.menus.lunch[0].name
       } else {
         var lunch = []
       };
-      if(restaurant.menus.dinner[0]) {
-        var lunch = restaurant.menus.dinner[0].name
+      if(this.restaurant.menus.dinner[0]) {
+        var lunch = this.restaurant.menus.dinner[0].name
       } else {
         var dinner = []
       };
       // PUSH the bkfst, lunch, and dinner arrays to the menuSearch array
+      console.log(menuSearch)
       menuSearch.push(bkfst, lunch, dinner);
     };
     // CALL the function checkMenu
-    checkMenu(restaurant)
+    checkMenus(foodItem) {*/
+
     // IF the menuSearch array INCLUDES the name property of the foodItem
-    if(menuSearch.includes(foodItem.name)) {
+    // if(this.restaurant.menus.breakfast.includes(foodItem) || this.restaurant.menus.lunch.includes(foodItem) || this.restaurant.menus.dinner.includes(foodItem)) {
+    //   return `Yes, we're serving ${foodItem.name} today!`
+    // } else {
+    //   return `Sorry, we aren't serving ${foodItem.name} today.`
+    //}
+    //if(menuSearch.includes(foodItem.name)) {
       // RETURN INSTANTIATED string
-      return `Yes, we're serving ${foodItem.name} today!`
+      //return `Yes, we're serving ${foodItem.name} today!`
       // ELSE RETURN other INSTANTIATED string
-    } else {
-      return `Sorry, we aren't serving ${foodItem.name} today.`
-    };
-  };
-}
+    //} else {
+      //return `Sorry, we aren't serving ${foodItem.name} today.`
 
 module.exports = Chef;
