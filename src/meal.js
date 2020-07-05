43c12142e7d8cdfe986474d51132bac7d8b4ad2f
Kristi Miller <kristi.ann.miller@gmail.com>
2020-07-05 13:31:28 -0600

    Final adjustment meal.js

--- src/meal.js
+++ src/meal.js
@@ -3,6 +3,7 @@ function nameMenuItem(food) {
   // return the string "Delicious <food>"
   return `Delicious ${food}`
 };
+
 // create a new FUNCTION called createMenuItem that pulls in menuItemName, price, and type params
 function createMenuItem(menuItemName, price, type) {
   // return a new OBJECT using the createMenuItem params
@@ -12,27 +13,26 @@ function createMenuItem(menuItemName, price, type) {
     type: type
   };
 };
-// create a new FUNCTION called addIngredients that pulls in ingredient and the name of the array as params
+
+// create a FUNCTION called addIngredients that pulls in ingredient and the name of the array as params
 function addIngredients(ingredient, array) {
   // IF the ingredient param already exists in the array, don't add it
   // ELSE PUSH the ingredient into the array param
-  /* if(array.includes(ingredient)) {
-      return
-    } else {
-      array.push(ingredient);
-    } */
   array.includes(ingredient) ? null : array.push(ingredient)
 };
+
 // create a FUNCTION called formatPrice that pulls in an initialPrice param
 function formatPrice(initialPrice) {
-  // RETURN the initialPrice param with a $ in front of it as a string (concatenation?)
+  // RETURN the initialPrice param concatenated to be $0.00 format
   return "$" + initialPrice
 };
+
 // create a FUNCTION called decreasePrice that pulls in a price param
 function decreasePrice(price) {
-  // return price minus (price*.1)
+  // return price minus 10% of the price
   return price - (price * .1);
 };
+
 //create a FUNCTION called createRecipe that pulls in title, ingredients, and menuItemType as params
 function createRecipe(title, ingredients, menuItemType) {
   // RETURN an OBJECT using the createRecipe params
