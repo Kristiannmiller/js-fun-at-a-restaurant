8be4f1d8ef5152ba910844b56c8ef2d718c3a221
Kristi Miller <kristi.ann.miller@gmail.com>
2020-07-05 16:05:26 -0600

    Final adjustment chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -30,47 +30,5 @@ class Chef {
     };
   };
 };
-    // create empty ARRAY to push to
-    // var menuSearch = []
-    // create FUNCTION to check through the menu of the restaurant param
-    // function checkMenus(foodItem) {
-      // IF there is a value within the breakfast menu of the object
-      /*if(this.restaurant.menus.breakfast) {
-        // assign the variable bkfst to the name property of that value
-        var bkfst = this.restaurant.menus.breakfast[0].name
-        // ELSE assign the variable bkfst to an empty array
-      } else {
-        var bkfst = []
-      };
-      // repeat IF/ELSE statments for lunch and dinner menus
-      if(this.restaurant.menus.lunch[0]) {
-        var lunch = this.restaurant.menus.lunch[0].name
-      } else {
-        var lunch = []
-      };
-      if(this.restaurant.menus.dinner[0]) {
-        var lunch = this.restaurant.menus.dinner[0].name
-      } else {
-        var dinner = []
-      };
-      // PUSH the bkfst, lunch, and dinner arrays to the menuSearch array
-      console.log(menuSearch)
-      menuSearch.push(bkfst, lunch, dinner);
-    };
-    // CALL the function checkMenu
-    checkMenus(foodItem) {*/
-
-    // IF the menuSearch array INCLUDES the name property of the foodItem
-    // if(this.restaurant.menus.breakfast.includes(foodItem) || this.restaurant.menus.lunch.includes(foodItem) || this.restaurant.menus.dinner.includes(foodItem)) {
-    //   return `Yes, we're serving ${foodItem.name} today!`
-    // } else {
-    //   return `Sorry, we aren't serving ${foodItem.name} today.`
-    //}
-    //if(menuSearch.includes(foodItem.name)) {
-      // RETURN INSTANTIATED string
-      //return `Yes, we're serving ${foodItem.name} today!`
-      // ELSE RETURN other INSTANTIATED string
-    //} else {
-      //return `Sorry, we aren't serving ${foodItem.name} today.`
 
 module.exports = Chef;
